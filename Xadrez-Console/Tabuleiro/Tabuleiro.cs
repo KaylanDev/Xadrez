f0dbeb0f559fdde6aa11cc0084f95183d2a8b751
KaylanDev <k1ndealblack@gmail.com>
2024-10-29 00:52:39 -0300

    tratando erros com excessao personalizada

--- Xadrez-Console/Tabuleiro/Tabuleiro.cs
+++ Xadrez-Console/Tabuleiro/Tabuleiro.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xadrez_Console.tabuleiro;
 
 namespace tabuleiro
 {
@@ -17,13 +18,47 @@ namespace tabuleiro
         {
             Linha = linha;
             Colunas = colunas;
-            pecas = new Peca[linha,colunas];
+            pecas = new Peca[linha, colunas];
         }
 
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.Linha, pos.Coluna];
+        }
         public void ColocarPeca(Peca p, Posicao pos)
         {
+            if (existirPeca(pos))
+            {
+                throw new TabuleiroExeption("Ja existe uma peça nessa posição!");
+            }   
             pecas[pos.Linha, pos.Coluna] = p;
             p.posicao = pos;
         }
+
+        public bool existirPeca(Posicao pos)
+        {
+            validadePosicao(pos);
+            return peca(pos) != null;
+        }
+
+        public bool posicaoValida(Posicao pos)
+        {
+            if (pos.Linha < 0 || pos.Linha >= Linha || pos.Coluna < 0 || pos.Coluna >= Colunas)
+            {
+                return false;
+            }
+
+
+            return true;
+
+        }
+
+        public void validadePosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos)) {
+                throw new TabuleiroExeption("Posição Invalida!");
+            }
+        }
+
     }
 }
---
+++ Xadrez-Console/Tabuleiro/TabuleiroExeption.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xadrez_Console.tabuleiro
+{
+    internal class TabuleiroExeption : Exception
+    {
+        public TabuleiroExeption(string msg) : base(msg) {
+        
+        }
+    }
+}
